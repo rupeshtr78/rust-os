c86b9f0652d5577e25f89b9ef9a102fe98d0c46c
rupesh-raghavan <rupesh_raghavan@apple.com>
2025-06-03 16:24:23 -0700

    add tests

--- rust-os-v1/src/main.rs
+++ rust-os-v1/src/main.rs
@@ -1,15 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+mod serial;
 mod vga_buffer;
-/// This function is called on panic.
+
+#[cfg(not(test))] // This is only for the normal build, not for tests.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+// This function is called on panic in tests.
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// static HELLO: &[u8] = b"Hello World!";
 
 /// First, we cast the integer 0xb8000 into a raw pointer.
 /// Then we iterate over the bytes of the static HELLO byte string.
@@ -29,7 +45,69 @@ pub extern "C" fn _start() -> ! {
     //     }
     // }
 
-    vga_buffer::write_something();
+    // vga_buffer::write_something();
+
+    // use core::fmt::Write;
+    // vga_buffer::WRITER
+    //     .lock()
+    //     .write_str("Hello again")
+    //     .unwrap_err();
+
+    // write!(
+    //     vga_buffer::WRITER.lock(),
+    //     ", some numbers: {} {}",
+    //     42,
+    //     1.337
+    // )
+    // .unwrap_err();
+
+    // panic!("Some panic message");
+    println!("Hello World{}", "!");
+    println!("Hello Again {}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x010,
+    Failed = 0x11,
+}
+
+/// The function creates a new Port at 0xf4, which is the iobase of the isa-debug-exit device.
+/// Then it writes the passed exit code to the port.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("Trivial assertion test ...");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_serial_macro_fail() {
+    serial_println!("Test: {}", 42); // Should print "Test: 42"
+    assert_eq!(1, 1);
 }
